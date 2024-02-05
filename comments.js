29df4e5b33b816b728125b34d582d9e080c88850
Fernando <4560552+brilvio@users.noreply.github.com>
2024-02-05 15:19:55 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,43 @@
+// create web server
+// create api
+// create a route
+// create a controller
+// create a model
+// create a database
+
+// 1. create a web server
+const express = require('express');
+const app = express();
+
+// 2. create an api
+const comments = require('./comments');
+
+// 3. create a route
+app.get('/comments', (req, res) => {
+    comments.getComments((err, data) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.status(200).send(data);
+        }
+    });
+});
+
+app.get('/comments/:id', (req, res) => {
+    comments.getComment(req.params.id, (err, data) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.status(200).send(data);
+        }
+    });
+});
+
+// 4. create a controller
+// 5. create a model
+// 6. create a database
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
